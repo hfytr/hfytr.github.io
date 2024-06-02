e9d0e5bae86f042ee802b0c633d80d3235663167
Archim Jhunjhunwala <archim.jhunjhunwala@gmail.com>
2024-06-01 20:52:36 -0400

    changes

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -11,13 +11,17 @@ closeOnEscape={false} closeOnOutsideClick={false}>
     <Dialog.Content>
         <Dialog.Header>
             <Dialog.Title>
-                <h1 class="font-bold text-3xl text-center">
+                <h1 class="font-bold text-3xl text-center text-foreground">
                     Cookie Policy
                 </h1>
             </Dialog.Title>
             <Dialog.Description>
-                <div>
+                <div class = "flex items-center justify-around">
                     Please accept cookies.
+                </div>
+                <div class = "flex items-center justify-around mt-4">
+                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="bg-background border-solid border-color border-2 rounded-lg px-4 py-2 w-48 text-center text-foreground">Accept</a>
+                    <a href="/cookies" class="bg-background border-solid border-color border-2 rounded-lg px-4 py-2 w-48 text-center text-foreground">Manage Preferences</a>
                 </div>
             </Dialog.Description>
         </Dialog.Header>
