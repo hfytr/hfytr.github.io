d683a974c85a1ed54cd54f1f09d0cf5802ef6f89
AlohaHawaii89 <alohahawaii899@gmail.com>
2024-06-02 16:06:06 -0400

    essay

---
+++ src/components/essay.svelte
@@ -0,0 +1,18 @@
+<script>
+    let text=""
+    $: words=text.split(" ").length
+</script>
+<h1>Usings at least 250 words, summarize the Communist Manifesto.</h1>
+<textarea bind:value={text} class="bg-slate-400"></textarea>
+<div>
+    {text == "" ? 0 : words} wordcount:
+</div>
+
+<button on:click={()=>{
+    if (words > 250){
+        location.href = "/clicker"
+    }
+    else {
+        alert("Not enough words")
+    }
+}}>Submit</button>
