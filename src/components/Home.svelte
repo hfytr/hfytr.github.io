270022ebec25e8511692935f402e9631f14f5570
TheLazySquid <thelazysquid2@gmail.com>
2024-06-01 20:04:07 -0400

    Home page start

---
+++ src/components/Home.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import * as Dialog from '$lib/components/ui/dialog';
+
+    let open = true;
+
+    $: if(!open) setTimeout(() => open = true, 50);
+</script>
+
+<Dialog.Root bind:open={open}
+closeOnEscape={false} closeOnOutsideClick={false}>
+    <Dialog.Content>
+        <Dialog.Header>
+            <Dialog.Title>
+                <h1 class="font-bold text-3xl text-center">
+                    Cookie Policy
+                </h1>
+            </Dialog.Title>
+            <Dialog.Description>
+                <div>
+                    Please accept cookies.
+                </div>
+            </Dialog.Description>
+        </Dialog.Header>
+    </Dialog.Content>
+</Dialog.Root>
