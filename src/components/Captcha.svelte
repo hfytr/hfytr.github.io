c9ed1c584cda86b7c82db3369cf556d0f42cecc0
Archim Jhunjhunwala <archim.jhunjhunwala@gmail.com>
2024-06-02 07:27:58 -0400

    captchas

---
+++ src/components/Captcha.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+    import * as Dialog from '$lib/components/ui/dialog';
+
+    let open = true;
+    let openWrong = false;
+    let src = "/images/orig.png";
+    let input = "";
+
+    $: if(!open) setTimeout(() => open = true, 50);
+</script>
+
+<Dialog.Root bind:open={open}
+closeOnEscape={false} closeOnOutsideClick={false}>
+    <Dialog.Content>
+        <Dialog.Header>
+            <Dialog.Title>
+                <h1 class="font-bold text-3xl text-center text-foreground">
+                    Are You A Robot?
+                </h1>
+            </Dialog.Title>
+            <Dialog.Description>
+            <img alt="Loating Captcha..." src={src}/>
+            <div class = "flex items-center justify-around mt-4">
+                <input class="mt-5" bind:value={input} />
+                <button class="bg-background border-solid border-color border-2 rounded-lg px-4 py-2 w-48 text-center text-foreground" on:click={() => {
+                    src = "/images/altered.png";
+                    if (input == "A1Z4q") {
+                        location.href = "/cookies";
+                    } else {
+                        openWrong = true;
+                    }
+                }}>
+                Submit
+                </button>
+            </div>
+            </Dialog.Description>
+        </Dialog.Header>
+    </Dialog.Content>
+</Dialog.Root>
+
+<Dialog.Root bind:open={openWrong}
+closeOnEscape={false} closeOnOutsideClick={false}>
+    <Dialog.Content>
+        <Dialog.Header>
+            <Dialog.Title>
+                <div class="min-h-36 content-center">
+                    <h1 class="font-bold text-3xl text-center text-foreground">
+                        Wrong Answer
+                    </h1>
+                </div>
+            </Dialog.Title>
+        </Dialog.Header>
+    </Dialog.Content>
+</Dialog.Root>
