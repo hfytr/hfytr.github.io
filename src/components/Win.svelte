ad683b9c93b1c3c4b62a3fe73a21a008b6aa4d6b
TheLazySquid <thelazysquid2@gmail.com>
2024-06-02 13:02:55 -0400

    Add win screen

---
+++ src/components/Win.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import JSConfetti from 'js-confetti';
+
+    const confetti = new JSConfetti();
+
+    confetti.addConfetti({
+        confettiNumber: 500
+    });
+</script>
+
+<div class="flex flex-col items-center h-full bg-cover bg-center"
+style="background-image: url('/winScreen.png')">
+    <h1 class="text-5xl font-bold">You did it!</h1>
+    <p class="mb-3 text-2xl">You've successfully disabled cookies. Good job!</p>
+    <a href="/" class="w-40 text-center bg-muted p-1 rounded-lg">
+        Start from beginning
+    </a>
+</div>
